feb78ac4477b45bcdccc7bb95ff3aab7cd77b40f
Motufero <saulogcf@gmail.com>
2023-11-27 11:37:41 -0300

    função de adicionar tarefas andamento

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -17,3 +17,25 @@ function addTask(){
         document.getElementById("demo").innerHTML = "Insira um nome!";
     }
 }
+
+function editNewTask(){
+    var e = document.getElementById('taskInfo');
+    while (e.firstChild) {
+        e.removeChild(e.firstChild);
+    }
+    let chdForm = document.createElement('form');
+    chdForm.method = "post";
+    chdForm.role = "form";
+
+    let chdLabelName = document.createElement('label');
+    chdLabelName.className = "form-group";
+    chdLabelName.innerHTML = `<label for=''>Nome da Tarefa</label>
+    <input type="text" class="form-control" name="name" placeholder="Digite o nome da tarefa" id = "nomeTask">`;
+
+    let chdLabelGeneric = document.createElement('label');
+    chdLabelGeneric.className = "form-group";
+    chdLabelGeneric.innerHTML = `<label for="">Assunto Geral</label>
+    <input type="text" class="form-control" name="subject" placeholder="Digite o tema da tarefa">`;
+
+    e.append(chdForm, chdLabelName, chdLabelGeneric);
+}
