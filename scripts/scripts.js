36ab789c060b2af04f3dbd0d8b1cf0b8a4b3aff5
Motufero <saulogcf@gmail.com>
2023-11-30 11:14:11 -0300

    correção nos botões do footer

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -12,6 +12,9 @@ function addTask(){
         newTask.innerHTML = textvalue;
         let todoList = document.getElementById('menubar');
         todoList.appendChild(newTask);
+        deactivateMain();
+        constructMainInfo();
+        constructFooter();
     }
     else{
         alert('Insira as informações da tarefa!');
@@ -45,11 +48,10 @@ function editNewTask(){
 
     let chdButton = document.createElement('button');
     chdButton.className = "button";
-    chdButton.innerHTML = `<button type="submit" class="button"
+    chdButton.innerHTML = `<button type="submit" class="mainButton"
     onclick="addTask()">Submit</button>`;
 
     mainForm.append(chdForm, chdLabelName, chdLabelGeneric, chdLabelDescription, chdButton);
-
 }
 
 function deactivateMain(){
@@ -64,4 +66,39 @@ function deactivateFooter(){
     while (ftr.firstChild) {
         ftr.removeChild(ftr.firstChild);
     }
+}
+
+function constructMainInfo(){
+    var mainInfo = document.getElementById('taskInfo');
+    let chdHeader = document.createElement('h2');
+    chdHeader.id = "headTask";
+    chdHeader.innerHTML = "Nome da Tarefa";
+    let chdDescription = document.createElement('p');
+    chdDescription.innerHTML = "Description";
+    mainInfo.append(chdHeader, chdDescription);
+}
+
+function constructFooter(){
+    var footer = document.getElementById('footer');
+    let chdButtonConcluir = document.createElement('button');
+    chdButtonConcluir.className = "ftrButton";
+    chdButtonConcluir.style="background-color: #4caf50";
+    chdButtonConcluir.textContent = "Concluir";
+    chdButtonConcluir.addEventListener('click', () => {
+        alert('concluir!');
+      });
+    
+    let chdButtonDeletar = document.createElement('button');
+    chdButtonDeletar.className = "ftrButton";
+    chdButtonDeletar.style="background-color:rgb(211, 73, 73)";
+    chdButtonDeletar.textContent = "Deletar";
+    chdButtonDeletar.addEventListener('click', () => {
+        alert('deletar!');
+    });
+
+    /*let chdButtonDeletar = document.createElement('button');
+    chdButtonDeletar.innerHTML = `<button type ="button" onclick="alert('Deletar')" 
+    class="ftrButton" style="background-color:rgb(211, 73, 73);">Deletar</button>`;
+    */
+    footer.append(chdButtonConcluir, chdButtonDeletar);
 }
